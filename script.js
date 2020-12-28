19e1baf10a8ba1c347378f36c0254196ef45b119
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2020-12-28 21:29:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,114 @@
-for (let i = 2 ; i < 10 ; i++){
-    if (i%2 == 0){
-        alert (i)
-    }
-}
+// let a = 2+2 
+// switch (a){
+//     case 3:
+//         alert ('нет')
+//         break;
+//         case 4:
+//             alert ('Хорошо')
+//             break;
+//             case 5:
+//                 alert ('много')
+//                 break;
+//                 defult:
+//                 alert('Нет такого значений')
+// }
+
+// let a ='1'
+// let b = 0
+
+// switch (+a){
+//     case b+1: 
+//     alert ('Выполниться, т.к. значением +а будет 1, что в точности равно B+1')
+//     break;
+
+//     defult:
+//     alert ('Это не выполниться ')
+// }
+
+// let a = 2+2
+// switch (a){
+//     case 4:
+//         alert('правильно')
+//         break;
+//         case 3:
+//             case 5 :
+//                 alert ('неправлиьно')
+//                 alert ('посети урок матетматики')
+//                 break;
+
+//                 defult:
+//                 alert ('Результат выглядит странновато. Честно.')
+// }
+
+// let age = prompt ('ведите значение')
+// switch (age){
+//     case '0':
+//     case '9':
+//     case '7':
+//         alert ('правильно')
+//         break;
+
+//         case '222':
+//         case '100':
+//             alert ('Не правильное ')
+//             break;
+
+//             case 3: 
+//         default:
+//             alert ('Не известное значение ')
+// }
+
+
+//  switch & if
+
+
+
+
+//  if (browser == 'Edge'){
+//      alert ("You've got the Edge!")
+//   }else if (browser == 'Chrome'
+//   || browser == 'Firefox'
+//   || browser == 'safari'
+//   || browser == 'Opera'){
+//       alert ('Okay we support these browser too')
+//   } else {
+//       alert ('We hope that this page looks ok!')
+//   }
+
+// if & switch 
+
+// const number =+prompt ('Введите число между 0 и 3',"")
+// if (number === 0){
+//     alert ('Ведите число 0')
+// }
+
+// if (number === 1){
+//     alert ('Вы ввидете число 1')
+// }
+
+// if (number === 2 || number === 3 ){
+//     alert ('Вы вели 2, а может и 3 ')
+// }
+
+
+// const number =+ prompt ('Введите число между 0 и 3',"")
+
+// switch (number){
+//     case 0:
+//         alert ('Вы ввели число 0')
+//         break;
+
+//         case 1:
+//             alert ('вы ввели число 1')
+//             break;
+
+//             case 2:
+//             case 3:
+//                 alert ('Вы ввели число 2, а может и 3')
+//                 break;
+//                 default:
+//                     alert ('Неизвестное число')
+    
+// }
+
+
